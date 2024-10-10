29d7f2b877cb7a915f9c1b385c24a02cd60ba53c
ydahal3 <ydahal3@gatech.edu>
2024-10-09 22:00:24 -0400

    removed music player

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import QuizBoard from "./components/QuizBoard";
-import MusicPlayer from "./common/MusicPlayer";
 import "./App.css";
 
 function App() {
-
   return (
-    <div className='App'>
-      <MusicPlayer />
+    <div className="App">
       <QuizBoard />
     </div>
   );
