7348bd432c48202785972c9d9f8d8c22e6053538
ydahal3 <ydahal3@gatech.edu>
2024-10-17 07:05:06 -0400

    Added routing for gh pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import QuizBoard from "./components/QuizBoard";
 import Uscis from "./components/Uscis";
 import "./App.css";
 
-// App component - update
 function App() {
   return (
     <Router>
@@ -12,10 +15,20 @@ function App() {
         <Routes>
           <Route exact path="/" element={<QuizBoard />} />
           <Route path="/uscis" element={<Uscis />} />
+          <Route path="*" element={<RedirectHandler />} />
         </Routes>
       </div>
     </Router>
   );
+}
+
+function RedirectHandler() {
+  const location = useLocation();
+  const redirectPath = new URLSearchParams(location.search).get("redirect");
+  if (redirectPath) {
+    window.history.replaceState(null, "", redirectPath);
+  }
+  return null;
 }
 
 export default App;
