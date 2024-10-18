c034c9da2f2c32af90aa3414596a5c16fce393e5
ydahal3 <ydahal3@gatech.edu>
2024-10-17 20:11:39 -0400

    added basename

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,19 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import QuizBoard from "./components/QuizBoard";
 import Uscis from "./components/Uscis";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="https://ydahal1.github.io/live-quiz/">
       <div className="App">
         <Routes>
           <Route exact path="/" element={<QuizBoard />} />
           <Route path="/uscis" element={<Uscis />} />
-          <Route path="*" element={<RedirectHandler />} />
         </Routes>
       </div>
     </Router>
   );
-}
-
-function RedirectHandler() {
-  const location = useLocation();
-  const redirectPath = new URLSearchParams(location.search).get("redirect");
-  if (redirectPath) {
-    window.history.replaceState(null, "", redirectPath);
-  }
-  return null;
 }
 
 export default App;
--- client/src/db/us_citizenship.js
+++ client/src/db/us_citizenship.js
@@ -1,245 +1,245 @@
 const usCitizenshipQuestions = [
-  // {
-  //   id: 1,
-  //   question: "What is the supreme law of the land?",
-  //   correctAnswer: ["The Constitution "],
-  //   questionAudioLength: 6000,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/01.mp3",
-  // },
-  // {
-  //   id: 2,
-  //   question: ["What does the Constitution do?"],
-  //   correctAnswer: [
-  //     "Sets up the government",
-  //     "Defines the government",
-  //     "Protects basic rights of Americans.",
-  //   ],
-  //   questionAudioLength: 5000,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/02.mp3",
-  // },
-  // {
-  //   id: 3,
-  //   question:
-  //     "The idea of self-government is in the first three words of the Constitution. What are these words?",
-  //   correctAnswer: ["We the People"],
-  //   questionAudioLength: 8000,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/03.mp3",
-  // },
-  // {
-  //   id: 4,
-  //   question: "What is an amendment?",
-  //   correctAnswer: [
-  //     "A change to the Constitution",
-  //     "An addition to the Constitution",
-  //   ],
-  //   questionAudioLength: 4000,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/04.mp3",
-  // },
-  // {
-  //   id: 5,
-  //   question: "What do we call the first ten amendments to the Constitution?",
-  //   correctAnswer: ["The Bill of Rights"],
-  //   questionAudioLength: 6000,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/05.mp3",
-  // },
-  // {
-  //   id: 6,
-  //   question: "What is one right or freedom from the First Amendment?",
-  //   correctAnswer: [
-  //     "Speech",
-  //     "Religion",
-  //     "Assembly",
-  //     "Press",
-  //     "Petition the Government",
-  //   ],
-  //   questionAudioLength: 6200,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/06.mp3",
-  // },
-  // {
-  //   id: 7,
-  //   question: "How many amendments does the Constitution have?",
-  //   correctAnswer: ["Twenty-seven (27)"],
-  //   questionAudioLength: 6000,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/07.mp3",
-  // },
-  // {
-  //   id: 8,
-  //   question: "What did the Declaration of Independence do?",
-  //   correctAnswer: [
-  //     "Announced our independence from Great Britain",
-  //     "Declared our independence from Great Britain",
-  //     "Said that the United States is free from Great Britain",
-  //   ],
-  //   questionAudioLength: 6000,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/08.mp3",
-  // },
-  // {
-  //   id: 9,
-  //   question: "What are two rights in the Declaration of Independence?",
-  //   correctAnswer: ["Life", "Liberty", "Pursuit of Happiness"],
-  //   questionAudioLength: 7000,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/09.mp3",
-  // },
-  // {
-  //   id: 10,
-  //   question: "What is freedom of religion?",
-  //   correctAnswer: [
-  //     "You can practice any religion, or not practice a religion",
-  //   ],
-  //   questionAudioLength: 5000,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/10.mp3",
-  // },
-  // {
-  //   id: 11,
-  //   question: "What is the economic system in the United States?",
-  //   correctAnswer: ["Capitalist economy", "Market economy"],
-  //   questionAudioLength: 6500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/11.mp3",
-  // },
-  // {
-  //   id: 12,
-  //   question: "What is the “rule of law”?",
-  //   correctAnswer: [
-  //     "Everyone must follow the law",
-  //     "Leaders must obey the law",
-  //     "Government must obey the law.",
-  //     "No one is above the law.",
-  //   ],
-  //   questionAudioLength: 5000,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/12.mp3",
-  // },
-  // {
-  //   id: 13,
-  //   question: "Name one branch or part of the government.",
-  //   correctAnswer: [
-  //     "The legislative branch (Congress)",
-  //     "The executive branch (President)",
-  //     "The judicial branch (Courts)",
-  //   ],
-  //   questionAudioLength: 6500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/13.mp3",
-  // },
-  // {
-  //   id: 14,
-  //   question: "What stops one branch of government from becoming too powerful?",
-  //   correctAnswer: ["Checks and balances", "Separation of powers"],
-  //   questionAudioLength: 6500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/14.mp3",
-  // },
-  // {
-  //   id: 15,
-  //   question: "Who is in charge of the executive branch?",
-  //   correctAnswer: ["The President"],
-  //   questionAudioLength: 7000,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/15.mp3",
-  // },
-  // {
-  //   id: 16,
-  //   question: "Who makes federal laws?",
-  //   correctAnswer: [
-  //     "Congress",
-  //     "Senate and House",
-  //     "(U.S. or national) legislature",
-  //   ],
-  //   questionAudioLength: 5500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/16.mp3",
-  // },
-  // {
-  //   id: 17,
-  //   question: "What are the two parts of the U.S. Congress?",
-  //   correctAnswer: ["The Senate", "House (of Representatives)"],
-  //   questionAudioLength: 6500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/17.mp3",
-  // },
-  // {
-  //   id: 18,
-  //   question: "How many U.S. Senators are there?",
-  //   correctAnswer: ["One hundred (100)"],
-  //   questionAudioLength: 5500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/18.mp3",
-  // },
-  // {
-  //   id: 19,
-  //   question: "We elect a U.S. Senator for how many years?",
-  //   correctAnswer: ["Six (6)"],
-  //   questionAudioLength: 5500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/19.mp3",
-  // },
-  // {
-  //   id: 20,
-  //   question: "Who is one of your state’s U.S. Senators now?",
-  //   correctAnswer: [
-  //     "Answers will vary. [District of Columbia residents and residents of U.S. territories should answer that D.C. (or the territory where the applicant lives) has no U.S. Senators.",
-  //   ],
-  //   questionAudioLength: 5500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/20.mp3",
-  // },
-  // {
-  //   id: 21,
-  //   question: "The House of Representatives has how many voting members?",
-  //   correctAnswer: ["Four hundred thirty-five (435)"],
-  //   questionAudioLength: 6500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/21.mp3",
-  // },
-  // {
-  //   id: 22,
-  //   question: "We elect a U.S. Representative for how many years?",
-  //   correctAnswer: ["2 Years"],
-  //   questionAudioLength: 6500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/22.mp3",
-  // },
-  // {
-  //   id: 23,
-  //   question: "Name your U.S. Representative.",
-  //   correctAnswer: [
-  //     "Answers will vary—You can find your U.S. Representative at house.gov by entering your zip code.",
-  //   ],
-  //   questionAudioLength: 5500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/23.mp3",
-  // },
-  // {
-  //   id: 24,
-  //   question: "Who does a U.S. Senator represent?",
-  //   correctAnswer: ["All people of the state"],
-  //   questionAudioLength: 5500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/24.mp3",
-  // },
-  // {
-  //   id: 25,
-  //   question: "Why do some states have more Representatives than other states?",
-  //   correctAnswer: [
-  //     "Because of the state’s population",
-  //     "Because they have more people",
-  //     "Because some states have more people",
-  //   ],
-  //   questionAudioLength: 6500,
-  //   questionAnswerAudio:
-  //     "https://quizstorageaccount.blob.core.windows.net/uscis/25.mp3",
-  // },
+  {
+    id: 1,
+    question: "What is the supreme law of the land?",
+    correctAnswer: ["The Constitution "],
+    questionAudioLength: 6000,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/01.mp3",
+  },
+  {
+    id: 2,
+    question: ["What does the Constitution do?"],
+    correctAnswer: [
+      "Sets up the government",
+      "Defines the government",
+      "Protects basic rights of Americans.",
+    ],
+    questionAudioLength: 5000,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/02.mp3",
+  },
+  {
+    id: 3,
+    question:
+      "The idea of self-government is in the first three words of the Constitution. What are these words?",
+    correctAnswer: ["We the People"],
+    questionAudioLength: 8000,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/03.mp3",
+  },
+  {
+    id: 4,
+    question: "What is an amendment?",
+    correctAnswer: [
+      "A change to the Constitution",
+      "An addition to the Constitution",
+    ],
+    questionAudioLength: 4000,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/04.mp3",
+  },
+  {
+    id: 5,
+    question: "What do we call the first ten amendments to the Constitution?",
+    correctAnswer: ["The Bill of Rights"],
+    questionAudioLength: 6000,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/05.mp3",
+  },
+  {
+    id: 6,
+    question: "What is one right or freedom from the First Amendment?",
+    correctAnswer: [
+      "Speech",
+      "Religion",
+      "Assembly",
+      "Press",
+      "Petition the Government",
+    ],
+    questionAudioLength: 6200,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/06.mp3",
+  },
+  {
+    id: 7,
+    question: "How many amendments does the Constitution have?",
+    correctAnswer: ["Twenty-seven (27)"],
+    questionAudioLength: 6000,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/07.mp3",
+  },
+  {
+    id: 8,
+    question: "What did the Declaration of Independence do?",
+    correctAnswer: [
+      "Announced our independence from Great Britain",
+      "Declared our independence from Great Britain",
+      "Said that the United States is free from Great Britain",
+    ],
+    questionAudioLength: 6000,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/08.mp3",
+  },
+  {
+    id: 9,
+    question: "What are two rights in the Declaration of Independence?",
+    correctAnswer: ["Life", "Liberty", "Pursuit of Happiness"],
+    questionAudioLength: 7000,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/09.mp3",
+  },
+  {
+    id: 10,
+    question: "What is freedom of religion?",
+    correctAnswer: [
+      "You can practice any religion, or not practice a religion",
+    ],
+    questionAudioLength: 5000,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/10.mp3",
+  },
+  {
+    id: 11,
+    question: "What is the economic system in the United States?",
+    correctAnswer: ["Capitalist economy", "Market economy"],
+    questionAudioLength: 6500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/11.mp3",
+  },
+  {
+    id: 12,
+    question: "What is the “rule of law”?",
+    correctAnswer: [
+      "Everyone must follow the law",
+      "Leaders must obey the law",
+      "Government must obey the law.",
+      "No one is above the law.",
+    ],
+    questionAudioLength: 5000,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/12.mp3",
+  },
+  {
+    id: 13,
+    question: "Name one branch or part of the government.",
+    correctAnswer: [
+      "The legislative branch (Congress)",
+      "The executive branch (President)",
+      "The judicial branch (Courts)",
+    ],
+    questionAudioLength: 6500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/13.mp3",
+  },
+  {
+    id: 14,
+    question: "What stops one branch of government from becoming too powerful?",
+    correctAnswer: ["Checks and balances", "Separation of powers"],
+    questionAudioLength: 6500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/14.mp3",
+  },
+  {
+    id: 15,
+    question: "Who is in charge of the executive branch?",
+    correctAnswer: ["The President"],
+    questionAudioLength: 7000,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/15.mp3",
+  },
+  {
+    id: 16,
+    question: "Who makes federal laws?",
+    correctAnswer: [
+      "Congress",
+      "Senate and House",
+      "(U.S. or national) legislature",
+    ],
+    questionAudioLength: 5500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/16.mp3",
+  },
+  {
+    id: 17,
+    question: "What are the two parts of the U.S. Congress?",
+    correctAnswer: ["The Senate", "House (of Representatives)"],
+    questionAudioLength: 6500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/17.mp3",
+  },
+  {
+    id: 18,
+    question: "How many U.S. Senators are there?",
+    correctAnswer: ["One hundred (100)"],
+    questionAudioLength: 5500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/18.mp3",
+  },
+  {
+    id: 19,
+    question: "We elect a U.S. Senator for how many years?",
+    correctAnswer: ["Six (6)"],
+    questionAudioLength: 5500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/19.mp3",
+  },
+  {
+    id: 20,
+    question: "Who is one of your state’s U.S. Senators now?",
+    correctAnswer: [
+      "Answers will vary. [District of Columbia residents and residents of U.S. territories should answer that D.C. (or the territory where the applicant lives) has no U.S. Senators.",
+    ],
+    questionAudioLength: 5500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/20.mp3",
+  },
+  {
+    id: 21,
+    question: "The House of Representatives has how many voting members?",
+    correctAnswer: ["Four hundred thirty-five (435)"],
+    questionAudioLength: 6500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/21.mp3",
+  },
+  {
+    id: 22,
+    question: "We elect a U.S. Representative for how many years?",
+    correctAnswer: ["2 Years"],
+    questionAudioLength: 6500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/22.mp3",
+  },
+  {
+    id: 23,
+    question: "Name your U.S. Representative.",
+    correctAnswer: [
+      "Answers will vary—You can find your U.S. Representative at house.gov by entering your zip code.",
+    ],
+    questionAudioLength: 5500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/23.mp3",
+  },
+  {
+    id: 24,
+    question: "Who does a U.S. Senator represent?",
+    correctAnswer: ["All people of the state"],
+    questionAudioLength: 5500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/24.mp3",
+  },
+  {
+    id: 25,
+    question: "Why do some states have more Representatives than other states?",
+    correctAnswer: [
+      "Because of the state’s population",
+      "Because they have more people",
+      "Because some states have more people",
+    ],
+    questionAudioLength: 6500,
+    questionAnswerAudio:
+      "https://quizstorageaccount.blob.core.windows.net/uscis/25.mp3",
+  },
   {
     id: 26,
     question: "We elect a President for how many years?",
