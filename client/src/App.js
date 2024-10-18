ed928cb3f112033e83bf910f395668e363a8f88a
ydahal3 <ydahal3@gatech.edu>
2024-10-17 20:34:08 -0400

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import { useEffect } from "react";
 import QuizBoard from "./components/QuizBoard";
 import Uscis from "./components/Uscis";
 import "./App.css";
+
+function RedirectHandler() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const redirect = params.get("redirect");
+    if (redirect) {
+      navigate(redirect);
+    }
+  }, [location, navigate]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router basename="/live-quiz">
       <div className="App">
+        <RedirectHandler />
         <Routes>
           <Route path="/" element={<QuizBoard />} />
           <Route path="/uscis" element={<Uscis />} />
